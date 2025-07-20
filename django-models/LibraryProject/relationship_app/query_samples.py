ab8a1e90b551adbbd0e111663c11305120285760
amine zarrouki <zarroukiamine@outlook.com>
2025-07-20 15:02:33 +0200

    zpibsrnze^zo

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,11 +9,12 @@ def get_books_in_library(library_name):
     return library.books.all()
 
 def get_librarian_for_library(library_name):
-    return Librarian.objects.get(library__name=library_name)
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)  # Exact requested pattern
 
-# Additional query examples demonstrating different approaches
-def get_books_by_author_alt(author_name):
-    return Book.objects.filter(author__name=author_name)
+# Alternative implementations
+def get_librarian_for_library_direct(library_name):
+    return Librarian.objects.get(library__name=library_name)
 
 def get_libraries_with_book(book_title):
     return Library.objects.filter(books__title=book_title)
