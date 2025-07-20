05f723464f3dfaf86db7e71da339f7038233eefc
amine zarrouki <zarroukiamine@outlook.com>
2025-07-20 19:03:34 +0200

    sneoc

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import user_passes_test, permission_required
 
-from .models import Book, Library, UserProfile 
+from .models import Library
+from .models import Book, UserProfile 
 from .forms import BookForm
 
 # Book list
